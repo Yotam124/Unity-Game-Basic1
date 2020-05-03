46c4e434f66061b86a7309cd34133b44dbba6a94
Yotam_d <44746421+Yotam124@users.noreply.github.com>
2020-05-03 23:15:26 +0300

    Spaceship

---
+++ 02-collisions/Assets/Scripts/3-collisions/DestroyOnTrigger2D.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * This component destroys its object whenever it triggers a 2D collider with the given tag.
+ */
+public class DestroyOnTrigger2D : MonoBehaviour {
+    [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
+    [SerializeField] string triggeringTag;
+    [SerializeField] HitPointsCounter hitPoints;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == triggeringTag && enabled) {
+            if (this.tag == "Player")
+            {
+                hitPoints.SetNumber(hitPoints.GetNumber() - 1);
+                if (hitPoints.GetNumber() == 0)
+                {
+                    Destroy(this.gameObject);
+                }
+                Destroy(other.gameObject);
+            }
+            else
+            {
+                Destroy(this.gameObject);
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
