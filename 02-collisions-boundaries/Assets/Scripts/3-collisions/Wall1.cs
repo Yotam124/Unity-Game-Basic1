608a930208e4b1a235b4ed87cc559b19521d2c01
Tomer <tomerhazan22@gmail.com>
2020-05-06 01:00:24 +0300

    spaceship-boundaries
    blabla
    
    Co-Authored-By: Yotam_d <yotam124@users.noreply.github.com>

---
+++ 02-collisions-boundaries/Assets/Scripts/3-collisions/Wall1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall1 : MonoBehaviour
+{
+    [SerializeField] string triggeringTag;
+    Vector3 po;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == triggeringTag && enabled)
+        {
+            po.y = other.gameObject.transform.position.y;
+            if (po.y > this.transform.position.y)
+            {
+                Vector3 movementVector = new Vector3(transform.position.x, transform.position.y - 0.5f, 0);
+                transform.position = movementVector;
+            }
+            else
+            {
+                Vector3 movementVector = new Vector3(transform.position.x, transform.position.y + 0.5f, 0);
+                transform.position = movementVector;
+            }
+           
+            
+        }
+    }
+}
---
+++ 02-collisions-boundaries/Assets/Scripts/3-collisions/boundaries.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boundaries : MonoBehaviour{
+
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+
+    // Start is called before the first frame update
+    void Start() {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+    }
+
+    // Update is called once per frame
+    void LateUpdate() {
+        Vector3 po = transform.position;
+        po.x = Mathf.Clamp(po.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+        if (po.y < -6)
+        {
+            po.y = 6.586f;
+            transform.position = po;
+        }
+        if (po.y > 6.59)
+        {
+            po.y = -6f;
+            transform.position = po;
+        }
+        //po.y = Mathf.Clamp(po.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+        transform.position = po;
+    }
+}
